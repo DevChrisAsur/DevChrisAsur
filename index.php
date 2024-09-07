507ac2d7492611497895fc4407c4e51f9e5ec36e
DevChris <developer@asjurfinlegaltech.com>
2024-09-07 12:52:27 -0500

    feacture-area_derecho: actualizacion de rutas

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once "controladores/plantilla.controlador.php";
 require_once "controladores/usuarios.controlador.php";
 require_once "controladores/areas.controlador.php";
+require_once "controladores/areasderecho.controlador.php";
 require_once "controladores/sedes.controlador.php";
 require_once "controladores/clientes.controlador.php";
 
@@ -10,6 +11,7 @@ require_once "controladores/clientes.controlador.php";
 require_once "modelos/clientes.modelo.php";
 require_once "modelos/usuarios.modelo.php";
 require_once "modelos/areas.modelo.php";
+require_once "modelos/areaderecho.modelo.php";
 require_once "modelos/sedes.modelo.php";
 
 
