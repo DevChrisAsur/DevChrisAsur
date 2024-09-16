c5401575f1a3a78f813e32e89a9d68c0ee1c40f6
DevChris <developer@asjurfinlegaltech.com>
2024-09-15 22:55:59 -0500

    feacture-areaderecho: actualizacion hash password

--- controladores/areasderecho.controlador.php
+++ controladores/areasderecho.controlador.php
@@ -1,16 +1,18 @@
 <?php
 
-class ControladorAreaDerecho{
+class ControladorAreaDerecho
+{
 
 	/*=============================================
 	CREAR CATEGORIAS
 	=============================================*/
 
-	static public function ctrCrearAreaDerecho(){
+	static public function ctrCrearAreaDerecho()
+	{
 
-		if(isset($_POST["nuevaAreaDerecho"])){
+		if (isset($_POST["nuevaAreaDerecho"])) {
 
-			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["nuevaAreaDerecho"])){
+			if (preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["nuevaAreaDerecho"])) {
 
 				$tabla = "area_derecho";
 
@@ -18,9 +20,9 @@ class ControladorAreaDerecho{
 
 				$respuesta = ModeloAreas::mdlIngresarAreas($tabla, $datos);
 
-				if($respuesta == "ok"){
+				if ($respuesta == "ok") {
 
-					echo'<script>
+					echo '<script>
 
 					swal({
 						  type: "success",
@@ -36,13 +38,10 @@ class ControladorAreaDerecho{
 								})
 
 					</script>';
-
 				}
+			} else {
 
-
-			}else{
-
-				echo'<script>
+				echo '<script>
 
 					swal({
 						  type: "error",
@@ -58,26 +57,112 @@ class ControladorAreaDerecho{
 						})
 
 			  	</script>';
-
 			}
-
 		}
-
 	}
 
 	/*=============================================
 	MOSTRAR CATEGORIAS
 	=============================================*/
 
-	static public function ctrVerAreasDerecho($item, $valor){
+	static public function ctrVerAreasDerecho($item, $valor)
+	{
 
 		$tabla = "area_derecho";
 
 		$respuesta = ModeloAreasDerecho::mdlVerAreaDerecho($tabla, $item, $valor);
 
 		return $respuesta;
-	
 	}
 
 
+	static public function ctrEditarAreaDerecho(){
+
+		if(isset($_POST["editarNombreAreaDerecho"])){
+	
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ-]+$/', $_POST["editarNombreAreaDerecho"])){
+	
+				$tabla = "area_derecho";
+	
+				$datos = array(
+					"law_area" => $_POST["editarNombreAreaDerecho"],
+					"id_area" => $_POST["idAreasDerecho"]
+				);
+	
+				$respuesta = ModeloAreasDerecho::mdlEditarAreaDerecho($tabla, $datos);
+	
+				if($respuesta == "ok"){
+	
+					echo '<script>
+						swal({
+							type: "success",
+							title: "El curso ha sido cambiado correctamente",
+							showConfirmButton: true,
+							confirmButtonText: "Cerrar"
+						}).then(function(result){
+							if (result.value) {
+								window.location = "areas";
+							}
+						});
+					</script>';
+	
+				}
+	
+			}else{
+	
+				echo '<script>
+					swal({
+						type: "error",
+						title: "El curso no puede ir vacío o llevar caracteres especiales!",
+						showConfirmButton: true,
+						confirmButtonText: "Cerrar"
+					}).then(function(result){
+						if (result.value) {
+							window.location = "areas";
+						}
+					});
+				</script>';
+	
+			}
+	
+		}
+	
+	}
+	
+	
+	/*=============================================
+	BORRAR CATEGORIA
+	=============================================*/
+
+	static public function ctrBorrarAreaDerecho(){
+
+		if(isset($_GET["idAreasDerecho"])){
+
+			$tabla ="area_derecho";
+			$datos = $_GET["idAreasDerecho"];
+
+			$respuesta = ModeloAreasDerecho::mdlBorrarAreaDerecho($tabla, $datos);
+
+			if($respuesta == "ok"){
+
+				echo'<script>
+
+					swal({
+						  type: "success",
+						  title: "El curso ha sido borrada correctamente",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+									if (result.value) {
+
+									window.location = "areas";
+
+									}
+								})
+
+					</script>';
+			}
+		}
+		
+	}
 }
