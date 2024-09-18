e08525951034e77e53d91d15734ad3d646478f8c
DevChris <developer@asjurfinlegaltech.com>
2024-09-18 00:27:42 -0500

    feacture-usuarios: actualizacion ver asesores

--- modelos/usuarios.modelo.php
+++ modelos/usuarios.modelo.php
@@ -9,32 +9,37 @@ class ModeloUsuarios{
 	=============================================*/
 
 	static public function mdlMostrarUsuarios($tabla, $item, $valor){
-
 		if($item != null){
-
 			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item");
-
 			$stmt -> bindParam(":".$item, $valor, PDO::PARAM_STR);
-
 			$stmt -> execute();
-
 			return $stmt -> fetch();
-
 		}else{
-
 			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
-
 			$stmt -> execute();
-
 			return $stmt -> fetchAll();
-
 		}
-		
-
 		$stmt -> close();
-
 		$stmt = null;
 
+	}
+	/*=============================================
+	MOSTRAR ASESORES
+	=============================================*/
+	static public function mdlMostrarAsesores($tabla, $item, $valor) {
+		if ($item != null) {
+			// Aquí se añade la condición adicional de perfil = 'Asesor comercial'
+			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item AND perfil = 'Asesor comercial'");
+			$stmt->bindParam(":".$item, $valor, PDO::PARAM_STR);
+			$stmt->execute();
+			return $stmt->fetch();
+		} else {
+			// Aquí se añade la condición adicional de perfil = 'Asesor comercial'
+			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE perfil = 'Asesor comercial'");
+			$stmt->execute();
+			return $stmt->fetchAll();
+		}
+		$stmt = null;
 	}
  
 	/*=============================================
