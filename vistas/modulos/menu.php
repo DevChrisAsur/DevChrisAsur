4f861150988f4442f86dcd6628f1162bc524876c
DevChris <developer@asjurfinlegaltech.com>
2024-09-18 00:30:03 -0500

    feacture-usuarios: fixes menu para coordinador comercial

--- vistas/modulos/menu.php
+++ vistas/modulos/menu.php
@@ -163,28 +163,12 @@
 
                 echo '
                   <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-group"></i>
-                        <span>Ofertas</span>
-                        <span class="pull-right-container">
-                            <i class="fa fa-angle-left pull-right"></i>
-                        </span>
-                    </a>
-                    <ul class="treeview-menu">
-                        
-                        <li>
-                            <a href="servicios">
-                                <i class="fa fa-address-card"></i>
-                                <span>Productos</span>
+                    <li>
+                            <a href="usuarios">
+                                <i class="fa fa-book"></i>
+                                <span>Usuarios</span>
                             </a>
-                        </li>
-                        <li>
-                            <a href="suscripciones">
-                                <i class="fa fa-address-card"></i>
-                                <span>suscripciones</span>
-                            </a>
-                        </li>
-                    </ul>
+                    </li>
                 </li>';
             }
             ?>
