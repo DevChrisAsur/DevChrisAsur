cac21f127078f611deb347e2e7010f0c9b313a61
DevChris <developer@asjurfinlegaltech.com>
2024-09-18 22:20:55 -0500

    feacture-suscripciones: consulta de suscripcion,cliente,servicio

--- modelos/suscripciones.modelo.php
+++ modelos/suscripciones.modelo.php
@@ -40,29 +40,39 @@ class ModeloSuscripcion{
 	static public function mdlVerSuscripciones($tabla, $item, $valor){
 
 		if($item != null){
-
 			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item");
-
 			$stmt -> bindParam(":".$item, $valor, PDO::PARAM_STR);
-
 			$stmt -> execute();
-
 			return $stmt -> fetch();
-
 		}else{
-
 			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
-
 			$stmt -> execute();
-
 			return $stmt -> fetchAll();
-
 		}
-
 		$stmt -> close();
-
 		$stmt = null;
+	}
 
+		/*=============================================
+	MOSTRAR SUSCRIPCIONES
+	=============================================*/
+	static public function mdlVerSuscripcionCliente($item, $valor) {
+		$query = "SELECT 
+					s.id_suscripcion,
+					s.suscripcion_status, s.start_date, s.end_date, s.id_service,
+					c.id_customer, 
+					c.first_name, c.last_name,
+					sv.service_name
+					FROM suscripcion s
+						INNER JOIN cliente c ON s.id_customer = c.id_customer
+						INNER JOIN servicio sv ON s.id_service = sv.id_service;
+					";	
+		$stmt = Conexion::conectar()->prepare($query);	
+		if ($item !== null && $valor !== null) {
+			$stmt->bindParam(":$item", $valor, PDO::PARAM_STR);
+		}	
+		$stmt->execute();
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 
 }
