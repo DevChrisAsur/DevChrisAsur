75639bbd0c3edde1ba5f74e2a00f363760e8806b
DevChris <developer@asjurfinlegaltech.com>
2024-10-09 23:26:53 -0500

    feacture-clientes:(actualizado) modelo para ver la informacion adicional del cliente

--- modelos/clientes.modelo.php
+++ modelos/clientes.modelo.php
@@ -57,6 +57,22 @@ static public function mdlVerClientes($tabla){
 
 }
 
+static public function mdlVerInfoCliente($tabla, $item, $valor){
+    if ($item != null) {
+        // Incluimos la cláusula WHERE
+        $stmt = Conexion::conectar()->prepare(  
+            "SELECT id_customer, cc, country, first_name, last_name, state, city, customer_type, email, phone, id_lead FROM $tabla WHERE $item = :$item");
+        $stmt->bindParam(":" . $item, $valor, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetch();
+    } else {
+        $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+    $stmt->close();
+    $stmt = null;
+}
 
     static public function mdlVerificarUsuario($tabla, $usuario) {
         try {
