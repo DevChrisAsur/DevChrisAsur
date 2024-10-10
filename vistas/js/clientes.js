bd4967e75bf2ce08511b33349fd9ca6f663b7eb9
DevChris <developer@asjurfinlegaltech.com>
2024-10-09 23:27:23 -0500

    feacture-clientes:(actualizado) evento para activar el box-body secundario con la informacion adicional del usuario

--- vistas/js/clientes.js
+++ vistas/js/clientes.js
@@ -25,3 +25,46 @@ $(".tablas").on("click", ".btnEliminarCliente", function(){
     })
 
 })
+
+$(document).on('click', '#btnInformacionAdicional', function() {
+
+    var idCliente = $(this).attr("idCliente"); // Recogemos el atributo idCliente
+    console.log("idCliente:", idCliente); // Depuración
+
+    if (!idCliente) {
+        console.error("No se encontró idCliente en el botón.");
+        return;
+    }
+
+    // Crear un FormData para pasar los datos por AJAX
+    var datos = new FormData();
+    datos.append("idCliente", idCliente);
+
+    // Hacer la solicitud AJAX
+    $.ajax({
+        url: "ajax/clientes.ajax.php", // Asegúrate de apuntar al archivo correcto
+        method: 'POST',
+        data: datos,
+        cache: false,
+        contentType: false,
+        processData: false,
+        dataType: 'json',
+        success: function(respuesta) {
+            console.log(respuesta);
+            if (respuesta.error) {
+                console.error("Error:", respuesta.error);
+                alert("Error: " + respuesta.error);
+            } else {
+                var nombreCompleto = respuesta.first_name + ' ' + respuesta.last_name;
+                $('#infoIdCliente').val(respuesta.cc);
+                $('#infoNombreApellido').text(nombreCompleto);
+                $('#infoEmail').text(respuesta.email);
+                $('#infoTelefono').text(respuesta.phone);
+                $('#infoIdLeads').text(respuesta.id_lead);
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.error('Error en la solicitud AJAX:', textStatus, errorThrown);
+        }
+    });
+});
