35acb00ab0450c3de4952da2ed7d0dcf2b17549b
DevChris <developer@asjurfinlegaltech.com>
2024-10-15 20:10:58 -0500

    format

--- modelos/servicios.modelo.php
+++ modelos/servicios.modelo.php
@@ -1,90 +1,82 @@
-<?php 
+<?php
 
 require_once "conexion.php";
 
-class ModeloServicios{
+class ModeloServicios
+{
 
 	/*=============================================
 	CREAR CATEGORIA
 	=============================================*/
 
-	static public function mdlRegistrarServicio($tabla, $datos){
+	static public function mdlRegistrarServicio($tabla, $datos)
+	{
 
-    $stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(service_name,service_price,num_people) VALUES (:service_name,:service_price,:num_people)");
+		$stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(service_name,service_price,num_people) VALUES (:service_name,:service_price,:num_people)");
 
-    $stmt->bindParam(":service_name", $datos['service_name'], PDO::PARAM_STR);
-    $stmt->bindParam(":service_price", $datos['service_price'], PDO::PARAM_STR);
-    $stmt->bindParam(":num_people", $datos['num_people'], PDO::PARAM_STR);
+		$stmt->bindParam(":service_name", $datos['service_name'], PDO::PARAM_STR);
+		$stmt->bindParam(":service_price", $datos['service_price'], PDO::PARAM_STR);
+		$stmt->bindParam(":num_people", $datos['num_people'], PDO::PARAM_STR);
 
-    if($stmt->execute()){
+		if ($stmt->execute()) {
 
-        return "ok";
+			return "ok";
+		} else {
 
-    }else{
+			return "error";
+		}
 
-        return "error";
-    
-    }
-
-    $stmt->close();
-    $stmt = null;
-
-}
+		$stmt->close();
+		$stmt = null;
+	}
 
 	/*=============================================
 	MOSTRAR CATEGORIAS
 	=============================================*/
 
-	static public function mdlVerServicios($tabla, $item, $valor){
+	static public function mdlVerServicios($tabla, $item, $valor)
+	{
 
-		if($item != null){
+		if ($item != null) {
 
 			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item");
 
-			$stmt -> bindParam(":".$item, $valor, PDO::PARAM_STR);
+			$stmt->bindParam(":" . $item, $valor, PDO::PARAM_STR);
 
-			$stmt -> execute();
+			$stmt->execute();
 
-			return $stmt -> fetch();
-
-		}else{
+			return $stmt->fetch();
+		} else {
 
 			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
 
-			$stmt -> execute();
+			$stmt->execute();
 
-			return $stmt -> fetchAll();
-
+			return $stmt->fetchAll();
 		}
 
-		$stmt -> close();
+		$stmt->close();
 
 		$stmt = null;
-
 	}
 
-    static public function mdlELiminarServicio($tabla, $datos){
+	static public function mdlELiminarServicio($tabla, $datos)
+	{
 
 		$stmt = Conexion::conectar()->prepare("DELETE FROM $tabla WHERE id_service = :id_service");
 
-		$stmt -> bindParam(":id_service", $datos, PDO::PARAM_INT);
+		$stmt->bindParam(":id_service", $datos, PDO::PARAM_INT);
 
-		if($stmt -> execute()){
+		if ($stmt->execute()) {
 
 			return "ok";
-		
-		}else{
+		} else {
 
-			return "error";	
-
+			return "error";
 		}
 
-		$stmt -> close();
+		$stmt->close();
 
 		$stmt = null;
-
 	}
-	
-
 }
-
